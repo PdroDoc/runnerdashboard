991367739a9c938a6263d32378526843eed6235b
Pdro DevLawyer <486888@gmail.com>
2025-05-31 00:45:25 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Pdro P - Runner App',
+  description: 'Runner Dashboard',
+  generator: 'Pdo P Dev Lawyer',
 }
 
 export default function RootLayout({
